2aaf34c5f5e8851c0d90e815dcc66939c00a4847
Raph33AI <raphnardone@gmail.com>
2026-01-02 07:09:33 +0000

    New

---
+++ alphavault-promo/src/Root.jsx
@@ -0,0 +1,17 @@
+import { Composition } from 'remotion';
+import { Video } from './Video';
+
+export const RemotionRoot = () => {
+  return (
+    <>
+      <Composition
+        id="AlphaVaultPromo"
+        component={Video}
+        durationInFrames={2100}
+        fps={60}
+        width={1920}
+        height={1080}
+      />
+    </>
+  );
+};
